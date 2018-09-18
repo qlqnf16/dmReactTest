0e28f493ef24a5fd340ab8f1fb0dccf8eb12bd34
qlqnf16 <lee.jeongmin1027@likelion.org>
2018-09-18 17:04:28 +0900

    footer 추가

---
+++ src/components/UI/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => (
+    <div className="fixed-bottom">
+        <div className="row">
+            <div className="col-8 row">
+                <li>이용약관</li>
+                <li>개인정보취급방침</li>
+                <li>결제서비스약관</li>
+                <li>FAQ</li>
+            </div>
+            <div className="col-4 text-right">
+                Copyright © DREAMARY. All rights reserved.
+            </div>
+        </div>
+    </div>
+)
+
+export default Footer
