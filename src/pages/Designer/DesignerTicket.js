9acd61bbb565942ac293df14701026fa4f38d221
SangWoo <sangw0804@likelion.org>
2018-11-02 22:43:04 +0900

    결제모듈 결제종류 radiobutton 추가

--- src/pages/Designer/DesignerTicket.js
+++ src/pages/Designer/DesignerTicket.js
@@ -21,7 +21,8 @@ class DesignerTicket extends Component {
         createdAt: 1540795759135,
         expiredAt: 20180811
       }
-    ]
+    ],
+    method: null
   };
 
   componentDidMount = async () => {
@@ -56,8 +57,8 @@ class DesignerTicket extends Component {
     IMP.init('imp06037656');
     IMP.request_pay(
       {
-        pg: 'danal', // version 1.1.0부터 지원.
-        pay_method: 'card',
+        pg: this.state.method === 'kakaopay' ? 'kakaopay' : 'danal', // version 1.1.0부터 지원.
+        pay_method: this.state.method,
         merchant_uid: 'merchant_' + new Date().getTime(),
         name: '주문명: 디자이너 이용권',
         amount: 500,
@@ -87,6 +88,10 @@ class DesignerTicket extends Component {
     );
   };
 
+  changeHandler = e => {
+    this.setState({ method: e.target.value });
+  };
+
   ticketActivate = async ticketId => {
     if (
       this.props.userData.expiredAt &&
@@ -113,15 +118,95 @@ class DesignerTicket extends Component {
               이용권 관리
             </div>
             <div className="dr_title mb-2">보유 이용권</div>
-            <div className="row">
-              <div className="col-4 py-2 mx-auto">
+            <div className="row" style={{ marginTop: '50px' }}>
+              <div
+                className="col-4 py-2 mx-auto"
+                style={{ display: 'flex', flexDirection: 'column' }}
+              >
+                <TicketPurchaseButton purchaseHandler={this.purchaseHandler} />
+                <div className="row">
+                  <div
+                    className="col-12 rf-tableBody"
+                    style={{ fontFamily: 'NanumSquareEB' }}
+                  />
+                  <div className="col-12 text-right rf-tableHead mb-3">
+                    결제방법
+                  </div>
+                  <div className="col-6">
+                    <input
+                      id="card"
+                      type="radio"
+                      name="method"
+                      value="card"
+                      onChange={this.changeHandler}
+                      checked={this.state.method === 'card'}
+                      style={{ marginRight: '3.5px', transform: 'scale(1.3)' }}
+                    />
+                    <label
+                      for="card"
+                      style={{ marginRight: '21.5px', fontSize: '1.5rem' }}
+                    >
+                      신용/체크카드
+                    </label>
+                  </div>
+                  <div className="col-6">
+                    <input
+                      id="trans"
+                      type="radio"
+                      name="method"
+                      value="trans"
+                      onChange={this.changeHandler}
+                      checked={this.state.method === 'trans'}
+                      style={{ marginRight: '3.5px', transform: 'scale(1.3)' }}
+                    />
+                    <label
+                      for="trans"
+                      style={{ marginRight: '21.5px', fontSize: '1.5rem' }}
+                    >
+                      실시간 계좌이체
+                    </label>
+                  </div>
+                  <div className="col-6">
+                    <input
+                      id="vbank"
+                      type="radio"
+                      name="method"
+                      value="vbank"
+                      onChange={this.changeHandler}
+                      checked={this.state.method === 'vbank'}
+                      style={{ marginRight: '3.5px', transform: 'scale(1.3)' }}
+                    />
+                    <label
+                      for="vbank"
+                      style={{ marginRight: '21.5px', fontSize: '1.5rem' }}
+                    >
+                      가상계좌
+                    </label>
+                  </div>
+                  <div className="col-6">
+                    <input
+                      id="kakao"
+                      type="radio"
+                      name="method"
+                      value="kakaopay"
+                      onChange={this.changeHandler}
+                      checked={this.state.method === 'kakaopay'}
+                      style={{ marginRight: '3.5px', transform: 'scale(1.3)' }}
+                    />
+                    <label
+                      for="kakao"
+                      style={{ marginRight: '21.5px', fontSize: '1.5rem' }}
+                    >
+                      카카오페이
+                    </label>
+                  </div>
+                </div>
                 {/* <TicketCounter
                   count={this.state.tickets.reduce(
                     (accu, pres) => (!pres.expiredAt ? accu + 1 : accu),
                     0
                   )}
                 /> */}
-                <TicketPurchaseButton purchaseHandler={this.purchaseHandler} />
               </div>
               <TicketBox
                 tickets={this.state.tickets}
