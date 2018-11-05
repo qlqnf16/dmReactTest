0ad5fdc97d0cd27bf62e8e7a65f0998efbf03a95
qlqnf16 <lee.jeongmin1027@likelion.org>
2018-11-05 19:47:45 +0900

    사전등록 alert hover 투명

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -61,22 +61,7 @@ class Toolbar extends Component {
           <NavbarBrand tag={Link} to={'/'}>
             <img className="logo" src={logo} alt="Main Logo" />
           </NavbarBrand>
-          <div
-            style={{
-              position: 'absolute',
-              display: 'flex',
-              zIndex: '1',
-              width: '280px',
-              padding: '1%',
-              backgroundColor: '#dd6866',
-              fontSize: '1.2rem',
-              fontWeight: 'bold',
-              color: 'white',
-              borderRadius: '5px',
-              left: '5%',
-              top: '85%'
-            }}
-          >
+          <div className="temporary_alert">
             <div style={{ marginRight: '5%', fontSize: '2.5rem' }}>⚠</div>{' '}
             디자이너 사전 등록 기간입니다.
             <br />
