527433f66ba2c4bcff6f0b45cad8c510cf7ce77b
SangWoo <sangw0804@likelion.org>
2018-11-05 19:29:25 +0900

    디자이너 등록 방법 S3로 이전

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -232,7 +232,7 @@ class Landing extends Component {
               <div style={{ display: 'flex' }}>
                 <a
                   style={{ marginRight: '1%', height: '48px' }}
-                  href="https://drive.google.com/open?id=1deM6NFlJ5VCTEj5dgjDenSIHw8y5S9Gx"
+                  href="	https://s3.ap-northeast-2.amazonaws.com/dreamary/%E1%84%83%E1%85%B5%E1%84%8C%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%82%E1%85%A5+%E1%84%83%E1%85%B3%E1%86%BC%E1%84%85%E1%85%A9%E1%86%A8+%E1%84%80%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B3.pdf"
                   target="_blank"
                   className="linkdeco"
                 >
