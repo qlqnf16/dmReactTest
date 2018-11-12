e0f8994532c47f7ad7684ca9e6d7ee5928775866
qlqnf16 <lee.jeongmin1027@likelion.org>
2018-11-12 14:41:54 +0900

    예약하기 css 수정, 라디오버튼 수정

--- src/components/ReservationForm/ReservationForm.js
+++ src/components/ReservationForm/ReservationForm.js
@@ -52,8 +52,8 @@ class ReservationForm extends Component {
           </div>
           <div className="row">
             <div className="col-2 text-right rf-tableHead">휴대폰 번호</div>
-            <div className="col-10 rf-tableBody row mx-0">
-              <span className="col-2 p-1">
+            <div className="col-10 rf-tableBody mx-0">
+              <span className="p-1 mr-3">
                 {typeof this.props.userData.phoneNumber === 'undefined'
                   ? null
                   : `${this.props.userData.phoneNumber.slice(
@@ -64,7 +64,7 @@ class ReservationForm extends Component {
                       7
                     )}-${this.props.userData.phoneNumber.slice(7, 11)}`}
               </span>
-              <span className=" font-weight-light col-7 p-1">
+              <span className=" font-weight-light p-1">
                 예약 정보는 휴대폰 번호로 전송됩니다
               </span>
             </div>
@@ -115,23 +115,23 @@ class ReservationForm extends Component {
           </div>
           <div className="row">
             <div className="col-2 text-right rf-tableHead">Point</div>
-            <div className="col-10 rf-tableBody row m-0">
+            <div className="col-10 rf-tableBody m-0">
               <input
                 onChange={e => this.props.handleInputChange(e)}
                 type="number"
                 name="point"
                 id="point"
-                className="rf-input col-3 mx-0"
+                className="rf-input mx-0"
                 value={this.props.state.point}
                 step="1000"
               />
               <button
                 onClick={this.props.pointSubmit}
-                className="rf-button col-1"
+                className="rf-button"
               >
                 적용
               </button>
-              <span className="font-weight-light col-7">
+              <span className="font-weight-light">
                 1,000 point 단위로 사용 가능합니다. 보유포인트 :{' '}
                 {this.props.userData.point}원
               </span>
@@ -152,42 +152,54 @@ class ReservationForm extends Component {
               className="col-10 rf-tableBody"
               style={{ fontFamily: 'NanumSquareEB' }}
             >
-              <input
-                type="radio"
-                name="method"
-                value="card"
-                onChange={this.props.handleInputChange}
-                checked={this.props.method === 'card'}
-                style={{ marginRight: '3.5px' }}
-              />
-              <span style={{ marginRight: '21.5px' }}>신용/체크카드</span>
-              <input
-                type="radio"
-                name="method"
-                value="trans"
-                onChange={this.props.handleInputChange}
-                checked={this.props.method === 'trans'}
-                style={{ marginRight: '3.5px' }}
-              />
-              <span style={{ marginRight: '21.5px' }}>실시간 계좌이체</span>
-              <input
-                type="radio"
-                name="method"
-                value="vbank"
-                onChange={this.props.handleInputChange}
-                checked={this.props.method === 'vbank'}
-                style={{ marginRight: '3.5px' }}
-              />
-              <span style={{ marginRight: '21.5px' }}>가상계좌</span>
-              <input
-                type="radio"
-                name="method"
-                value="kakaopay"
-                onChange={this.props.handleInputChange}
-                checked={this.props.method === 'kakaopay'}
-                style={{ marginRight: '3.5px' }}
-              />
-              카카오페이
+              <label className='rf-hideinput'>
+                <input
+                  type="radio"
+                  name="method"
+                  value="card"
+                  onChange={this.props.handleInputChange}
+                  checked={this.props.method === 'card'}
+                  style={{ marginRight: '3.5px' }}
+                />
+                <span className='rf-radiobutton'/> 
+                <span style={{ marginRight: '21.5px' }}>신용/체크카드</span>
+              </label>
+              <label className='rf-hideinput'>
+                <input
+                  type="radio"
+                  name="method"
+                  value="trans"
+                  onChange={this.props.handleInputChange}
+                  checked={this.props.method === 'trans'}
+                  style={{ marginRight: '3.5px' }}
+                />
+                <span className='rf-radiobutton'/> 
+                <span style={{ marginRight: '21.5px' }}>실시간 계좌이체</span>
+              </label>
+              <label className='rf-hideinput'>
+                <input
+                  type="radio"
+                  name="method"
+                  value="vbank"
+                  onChange={this.props.handleInputChange}
+                  checked={this.props.method === 'vbank'}
+                  style={{ marginRight: '3.5px' }}
+                />
+                <span className='rf-radiobutton'/> 
+                <span style={{ marginRight: '21.5px' }}>가상계좌</span>
+              </label>
+              <label className='rf-hideinput'>
+                <input
+                  type="radio"
+                  name="method"
+                  value="kakaopay"
+                  onChange={this.props.handleInputChange}
+                  checked={this.props.method === 'kakaopay'}
+                  style={{ marginRight: '3.5px' }}
+                />
+                <span className='rf-radiobutton'></span>
+                카카오페이
+              </label>
             </div>
           </div>
         </div>
