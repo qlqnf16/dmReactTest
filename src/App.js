a0400578283bb645496d51b1ce94095f1d8250f7
Woonjang Ahn <ahnroonjang@korea.ac.kr>
2018-11-13 16:40:00 +0900

    ie면 alert 띄움

--- src/App.js
+++ src/App.js
@@ -108,6 +108,15 @@ class App extends Component {
 
   componentDidMount = () => {
     if (!this.state.madeRequest) this.authListener();
+
+    // ie 브라우저인지 확인
+    let isIE =
+      navigator.userAgent.indexOf('MSIE ') > -1 ||
+      navigator.userAgent.indexOf('Trident/') > -1;
+    if (isIE)
+      alert(
+        '드리머리 서비스는 인터넷 익스플로러 환경에서 원활하지 않을 수 있습니다. 크롬이나 사파리 등 다른 브라우저 또는 모바일 버전을 이용해주세요.'
+      );
   };
 
   authListener() {
@@ -499,33 +508,55 @@ class App extends Component {
               />
 
               {/* 관리자페이지 */}
-              <Route 
-                path='/admin/userlist' 
-                component={this.props.userData.isAdmin ? M_AdminUserList : M_WrongAccess}
+              <Route
+                path="/admin/userlist"
+                component={
+                  this.props.userData.isAdmin ? M_AdminUserList : M_WrongAccess
+                }
               />
-              <Route 
-                path='/admin/designerlist' 
-                component={this.props.userData.isAdmin ? M_AdminDesignerList : M_WrongAccess}
+              <Route
+                path="/admin/designerlist"
+                component={
+                  this.props.userData.isAdmin
+                    ? M_AdminDesignerList
+                    : M_WrongAccess
+                }
               />
-              <Route 
-                path='/admin/makecoupon' 
-                component={this.props.userData.isAdmin ? M_AdminMakeCoupon : M_WrongAccess}
+              <Route
+                path="/admin/makecoupon"
+                component={
+                  this.props.userData.isAdmin
+                    ? M_AdminMakeCoupon
+                    : M_WrongAccess
+                }
               />
-              <Route 
-                path='/admin/noshow' 
-                component={this.props.userData.isAdmin ? M_AdminNoShow : M_WrongAccess}
+              <Route
+                path="/admin/noshow"
+                component={
+                  this.props.userData.isAdmin ? M_AdminNoShow : M_WrongAccess
+                }
               />
-              <Route 
-                path='/admin/qna' 
-                component={this.props.userData.isAdmin ? M_AdminQnA : M_WrongAccess}
+              <Route
+                path="/admin/qna"
+                component={
+                  this.props.userData.isAdmin ? M_AdminQnA : M_WrongAccess
+                }
               />
-              <Route 
-                path='/admin/reservationlist' 
-                component={this.props.userData.isAdmin ? M_AdminReservationList : M_WrongAccess}
+              <Route
+                path="/admin/reservationlist"
+                component={
+                  this.props.userData.isAdmin
+                    ? M_AdminReservationList
+                    : M_WrongAccess
+                }
               />
-              <Route 
-                path='/admin/waitinglist' 
-                component={this.props.userData.isAdmin ? M_AdminWaitingList : M_WrongAccess}
+              <Route
+                path="/admin/waitinglist"
+                component={
+                  this.props.userData.isAdmin
+                    ? M_AdminWaitingList
+                    : M_WrongAccess
+                }
               />
 
               {/* customer my page (for testing MyPageNavigationBar) */}
