98d62ffe257c80d3b50e764fd497784fd3101867
Woonjang Ahn <ahnroonjang@korea.ac.kr>
2018-11-17 17:52:06 +0900

    지역/샵주소 설명 추가

--- src/components/InfoForm/InfoForm.js
+++ src/components/InfoForm/InfoForm.js
@@ -281,18 +281,20 @@ class InfoForm extends Component {
           {this.props.isRegister}
         </FormGroup>
         <FormGroup row>
-          <div className="col-3 if_head">지역/샵주소</div>
-          <div
-            style={{
-              fontSize: '1rem',
-              lineHeight: 1,
-              letterSpacing: '0rem',
-              fontWeight: 'normal'
-            }}
-          >
-            스케줄 생성 시 시술이 진행될 샵이
-            <br />
-            선택되므로 정확히 입력해주세요.
+          <div className="col-3 if_head">
+            지역/샵주소
+            <div
+              style={{
+                fontSize: '1rem',
+                lineHeight: 1,
+                letterSpacing: '0rem',
+                fontWeight: 'normal'
+              }}
+            >
+              스케줄 생성 시 시술이 진행될 샵이
+              <br />
+              선택되므로 정확히 입력해주세요.
+            </div>
           </div>
           <div className="col-9 ">
             {this.addressSelector()}
