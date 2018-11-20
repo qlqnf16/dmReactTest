ac719b2b4433e62a2a3502709fb744c6baf8789a
gyeol <gyeol1212@likelion.org>
2018-11-21 01:23:32 +0900

    IE 해결

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'babel-polyfill';
+
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
