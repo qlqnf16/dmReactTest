f38c78dd6691b122f48d9d947a46c084a90939c7
gyeol <gyeol1212@likelion.org>
2018-11-26 17:30:02 +0900

    인피니티 스크롤 완료

--- src/mobilePages/components/DesignerList/DesignerCardList.js
+++ src/mobilePages/components/DesignerList/DesignerCardList.js
@@ -1,80 +1,37 @@
 import React from 'react';
 import DesignerCard from './DesignerCard';
 
-class DesignerCardList extends React.Component {
-  state = {
-    recruits: this.props.recruits,
-    recruitsSeen: this.props.recruits.slice(0, 6),
-    i: 6
-  };
+const DesignerCardList = props => {
+  return (
+    <div style={containerStyle}>
+      {props.recruitsSeen ? (
+        props.recruitsSeen.map((recruit, key) => (
+          <DesignerCard recruit={recruit} key={key} />
+        ))
+      ) : (
+        <div
+          style={{
+            fontSize: '20px',
+            width: '100%',
+            textAlign: 'center',
+            padding: '20px',
+            color: 'rgba(0,0,0,0.5)',
+            height: '200px',
+            lineHeight: '300px',
+            borderRadius: '5px'
+          }}
+        >
+          검색 결과가 없습니다.
+        </div>
+      )}
 
-  componentDidMount = () => {
-    window.addEventListener('scroll', this.handleOnScroll);
-  };
-
-  componentWillUnmount = () => {
-    window.removeEventListener('scroll', this.handleOnScroll);
-  };
-
-  handleOnScroll = () => {
-    let scrollTop =
-      (document.documentElement && document.documentElement.scrollTop) ||
-      document.body.scrollTop;
-    let scrollHeight =
-      (document.documentElement && document.documentElement.scrollHeight) ||
-      document.body.scrollHeight;
-    let clientHeight =
-      document.documentElement.clientHeight || window.innerHeight;
-    let scrolledToBottom = Math.ceil(scrollTop + clientHeight) >= scrollHeight;
-
-    let recruitsSeen = this.state.recruitsSeen.concat(
-      this.state.recruits.slice(this.state.i, this.state.i + 6)
-    );
-
-    if (
-      scrolledToBottom &&
-      this.state.recruits.length > this.state.recruitsSeen.length
-    ) {
-      this.setState({
-        ...this.state,
-        recruitsSeen,
-        i: this.state.i + 6
-      });
-    }
-  };
-
-  render() {
-    return (
-      <div style={containerStyle}>
-        {this.state.recruitsSeen.length ? (
-          this.state.recruitsSeen.map((recruit, key) => (
-            <DesignerCard recruit={recruit} key={key} />
-          ))
-        ) : (
-          <div
-            style={{
-              fontSize: '20px',
-              width: '100%',
-              textAlign: 'center',
-              padding: '20px',
-              color: 'rgba(0,0,0,0.5)',
-              height: '200px',
-              lineHeight: '300px',
-              borderRadius: '5px'
-            }}
-          >
-            검색 결과가 없습니다.
-          </div>
-        )}
-
-        {/* 보기 좋게 X2 
+      {/* 보기 좋게 X2 
         {props.recruits.map((recruit, key) => (
           <DesignerCard recruit={recruit} key={key} />
         ))} */}
-      </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const containerStyle = {
   width: '85%',
--- src/mobilePages/views/DesignerList.js
+++ src/mobilePages/views/DesignerList.js
@@ -1,24 +1,62 @@
-import React, { Component } from "react";
-import axios from "../../config/Axios";
-import firebase from "../../config/Firebase";
+import React, { Component } from 'react';
+import axios from '../../config/Axios';
+import firebase from '../../config/Firebase';
 
-import Header from "../components/DesignerList/Header";
-import FilterButton from "../components/DesignerList/FilterButton";
-import Filter from "../components/DesignerList/Filter";
-import DesignerCardList from "../components/DesignerList/DesignerCardList";
+import Header from '../components/DesignerList/Header';
+import FilterButton from '../components/DesignerList/FilterButton';
+import Filter from '../components/DesignerList/Filter';
+import DesignerCardList from '../components/DesignerList/DesignerCardList';
 
-import "./Pages.css";
+import './Pages.css';
 
 class DesignerList extends Component {
   state = {
     recruits: [],
     madeRequest: false,
-    filterOn: false
+    filterOn: false,
+    i: 6
+    // recruitsSeen: this.recruits.slice(0, 6)
+  };
+
+  // componentDidMount = () => {
+  //   window.addEventListener('scroll', this.handleOnScroll);
+  // };
+
+  componentWillUnmount = () => {
+    window.removeEventListener('scroll', this.handleOnScroll);
+  };
+
+  handleOnScroll = () => {
+    let scrollTop =
+      (document.documentElement && document.documentElement.scrollTop) ||
+      document.body.scrollTop;
+    let scrollHeight =
+      (document.documentElement && document.documentElement.scrollHeight) ||
+      document.body.scrollHeight;
+    let clientHeight =
+      document.documentElement.clientHeight || window.innerHeight;
+    let scrolledToBottom = Math.ceil(scrollTop + clientHeight) >= scrollHeight;
+
+    let recruitsSeen = this.state.recruitsSeen.concat(
+      this.state.recruits.slice(this.state.i, this.state.i + 6)
+    );
+
+    if (
+      scrolledToBottom &&
+      this.state.recruits.length > this.state.recruitsSeen.length
+    ) {
+      setTimeout(() => {
+        this.setState({
+          recruitsSeen,
+          i: this.state.i + 6
+        });
+      }, 300);
+    }
   };
 
   componentDidMount = async () => {
     if (!this.state.madeRequest) {
-      const { data } = await axios.get("recruits");
+      const { data } = await axios.get('recruits');
       const filteredData = data.filter(
         d =>
           d._designer &&
@@ -38,7 +76,7 @@ class DesignerList extends Component {
     await firebase
       .database()
       .ref(`/users`)
-      .on("value", async res => {
+      .on('value', async res => {
         const filterAddresses = [];
         let filterSido = [];
         Object.values(res.val()).forEach(user => {
@@ -51,8 +89,11 @@ class DesignerList extends Component {
         });
         filterSido = new Set(filterSido);
         filterSido = [...filterSido].sort();
-        await this.setState({ filterAddresses, filterSido, madeRequest: true });
+        this.setState({ filterAddresses, filterSido, madeRequest: true });
       });
+    this.setState({ recruitsSeen: this.state.recruits.slice(0, 6) });
+    window.addEventListener('scroll', this.handleOnScroll);
+    console.log('1212');
   };
 
   filterToggle = () => {
@@ -68,12 +109,12 @@ class DesignerList extends Component {
   };
 
   getFilteredCards = async () => {
-    let must = "";
-    let gender = "";
-    let no = "";
-    let date = "";
-    let sido = "";
-    let sigungu = "";
+    let must = '';
+    let gender = '';
+    let no = '';
+    let date = '';
+    let sido = '';
+    let sigungu = '';
 
     if (this.state.sido) sido = `sido=${this.state.sido}&`;
     if (this.state.sigungu) sigungu = `sigungu=${this.state.sigungu}&`;
@@ -82,17 +123,17 @@ class DesignerList extends Component {
 
     if (this.state.gender) gender = `gender=${this.state.gender}&`;
 
-    if (this.state.cut === "100") must += "cut=1&";
-    else if (this.state.cut === "0") no += "cut=2&";
-    if (this.state.perm === "100") must += "perm=1&";
-    else if (this.state.perm === "0") no += "perm=2&";
-    if (this.state.dye === "100") must += "dye=1&";
-    else if (this.state.dye === "0") no += "dye=2&";
-    if (no === "cut=2&perm=2&dye=2&")
-      return alert("받으실 서비스를 선택해주세요");
+    if (this.state.cut === '100') must += 'cut=1&';
+    else if (this.state.cut === '0') no += 'cut=2&';
+    if (this.state.perm === '100') must += 'perm=1&';
+    else if (this.state.perm === '0') no += 'perm=2&';
+    if (this.state.dye === '100') must += 'dye=1&';
+    else if (this.state.dye === '0') no += 'dye=2&';
+    if (no === 'cut=2&perm=2&dye=2&')
+      return alert('받으실 서비스를 선택해주세요');
 
     const { data } = await axios.get(
-      "cards?" + must + no + gender + date + sido + sigungu
+      'cards?' + must + no + gender + date + sido + sigungu
     );
 
     let recruits = data.map(d => d._recruit);
@@ -112,7 +153,10 @@ class DesignerList extends Component {
       }
     });
 
-    this.setState({ recruits: uniqueRecruits });
+    this.setState({
+      recruits: uniqueRecruits,
+      recruitsSeen: uniqueRecruits.slice(0, 6)
+    });
     this.filterToggle();
   };
 
@@ -131,17 +175,17 @@ class DesignerList extends Component {
       <div
         onClick={() => window.location.reload()}
         style={{
-          width: "85%",
-          height: "33px",
-          borderRadius: "5px",
-          boxShadow: "0 3px 6px 0 rgba(0, 0, 0, 0.16)",
-          border: "solid 1px #dd6866",
-          color: "#dd6866",
-          fontWeight: "bold",
-          fontSize: "1.3rem",
-          textAlign: "center",
-          lineHeight: "33px",
-          margin: "2% 0 4% 0"
+          width: '85%',
+          height: '33px',
+          borderRadius: '5px',
+          boxShadow: '0 3px 6px 0 rgba(0, 0, 0, 0.16)',
+          border: 'solid 1px #dd6866',
+          color: '#dd6866',
+          fontWeight: 'bold',
+          fontSize: '1.3rem',
+          textAlign: 'center',
+          lineHeight: '33px',
+          margin: '2% 0 4% 0'
         }}
       >
         초기화
@@ -162,7 +206,10 @@ class DesignerList extends Component {
             on={this.state.filterOn}
           />
           {this.state.filterOn ? refreshButton : null}
-          <DesignerCardList recruits={this.state.recruits} />
+          <DesignerCardList
+            recruits={this.state.recruits}
+            recruitsSeen={this.state.recruitsSeen}
+          />
         </div>
       );
     } else {
