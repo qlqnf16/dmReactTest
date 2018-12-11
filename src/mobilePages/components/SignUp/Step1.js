ee9e2de744e28e38b2c47d5560d072b72c1e6cac
qlqnf16 <lee.jeongmin1027@likelion.org>
2018-12-11 18:31:33 +0900

    회원가입 stepper

--- src/mobilePages/components/SignUp/Step1.js
+++ src/mobilePages/components/SignUp/Step1.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Step1 = props => {
   return (
-    <div style={{ marginTop: '7rem' }}>
-      <div className="h2 row mb-5" style={{ fontSize: '25px' }}>
+    <div>
+      {/* <div className="h2 row mb-5" style={{ fontSize: '25px' }}>
         <div
           className="col-5 text-right"
           style={{ color: '#1f3354', fontWeight: 'bold' }}
@@ -14,31 +14,38 @@ const Step1 = props => {
         <div className="col-5 text-left" style={{ color: 'rgba(0,0,0,0.5)' }}>
           STEP 2
         </div>
-      </div>
-      <div className="if_detail">
+      </div> */}
+      <div className="if_detail d-flex mb-3">
         <input
           type="checkbox"
           id="phoneNumberAgree"
           name="phoneNumberAgree"
           onChange={e => props.inputChangeHandler(e)}
         />
-        <label htmlFor="phoneNumberAgree">
+        <label
+          style={{ fontSize: 12, color: 'black', marginLeft: 5 }}
+          htmlFor="phoneNumberAgree"
+        >
           휴대폰 번호 수집에 대한 개인정보 제공에 동의합니다.
         </label>
       </div>
-      <div
-        className="btn"
-        style={{
-          width: '20rem',
-          height: '5rem',
-          color: '#dd6866',
-          border: 'solid 1px #dd6866',
-          fontSize: '2rem',
-          lineHeight: 2
-        }}
-        onClick={() => props.phoneCert()}
-      >
-        인증
+      <div className="d-block text-center">
+        <div
+          className="btn"
+          style={{
+            textAlign: 'center',
+            margin: '0 auto',
+            padding: '0.3rem 2rem',
+            color: 'white',
+            backgroundColor: '#de6966',
+            boxShadow: '2px 3px 10px rgba(0,0,0,0.5)',
+            fontSize: '1.4rem',
+            lineHeight: 2
+          }}
+          onClick={() => props.phoneCert()}
+        >
+          휴대폰 인증
+        </div>
       </div>
     </div>
   );
