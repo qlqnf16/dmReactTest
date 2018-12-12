83f4f0a0072648f1b29150ce3c3fc4e41faef205
qlqnf16 <lee.jeongmin1027@likelion.org>
2018-12-12 10:49:37 +0900

    회원가입 stepper 웹 버젼 추가

--- src/components/SignUp/Step2.js
+++ src/components/SignUp/Step2.js
@@ -1,23 +1,68 @@
 import React from 'react';
 import Moment from 'react-moment';
 
-const Step1 = props => {
+const Step2 = props => {
   return (
     <div>
-      <div className="h2">회원가입</div>
-      <div className="h2">STEP 2</div>
+      {/* <div className="h2 row mb-5" style={{ fontSize: '25px' }}>
+        <div className="col-5 text-right" style={{ color: 'rgba(0,0,0,0.5)' }}>
+          STEP 1
+        </div>
+        <div className="col-2 text-center">></div>
+        <div
+          className="col-5 text-left"
+          style={{ color: '#1f3354', fontWeight: 'bold' }}
+        >
+          STEP 2
+        </div>
+      </div> */}
       <div>
-        <div>이름 : {props.state.name}</div>
+        {/* <div>이름 : {props.state.name}</div>
         <div>성별 : {props.state.gender}</div>
         <div>번호 : {props.state.phoneNumber}</div>
         <div>
           생년월일 :{' '}
           <Moment format="YYYY/MM/DD">{props.state.birth * 1000}</Moment>
+        </div> */}
+        <div className="row">
+          <div className="col-12 if_head_2 text-left">이름</div>
+          <div className="col-12">
+            <div
+              className="if_head_2 text-left if_input"
+              style={{
+                textAlign: 'left',
+                color: 'black',
+                marginBottom: 13,
+                fontWeight: 'normal',
+                backgroundColor: '#ddd'
+              }}
+            >
+              신한결
+            </div>
+          </div>
         </div>
         <div className="row">
-          <div className="col-2 if_head uif_head">이메일 주소</div>
-          <div className="col-10">
+          <div className="col-12 if_head_2 text-left">휴대폰 번호</div>
+          <div className="col-12">
+            <div
+              className="if_head_2 text-left if_input"
+              style={{
+                textAlign: 'left',
+                color: 'black',
+                marginBottom: 13,
+                fontWeight: 'normal',
+                backgroundColor: '#ddd'
+              }}
+            >
+              01036051212
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 if_head_2 text-left">이메일 주소</div>
+          <div className="col-12">
             <input
+              style={{ color: 'black', marginBottom: 13, fontWeight: 'normal' }}
               type="email"
               name="email"
               id="email"
@@ -27,9 +72,10 @@ const Step1 = props => {
           </div>
         </div>
         <div className="row">
-          <div className="col-2 if_head uif_head">추천인 코드</div>
-          <div className="col-10">
+          <div className="col-12 if_head_2 text-left">추천인 코드</div>
+          <div className="col-12">
             <input
+              style={{ color: 'black', marginBottom: 13, fontWeight: 'normal' }}
               type="text"
               name="recommendationCode"
               id="recommendationCode"
@@ -38,11 +84,23 @@ const Step1 = props => {
             />
           </div>
         </div>
-        <div className="text-center">
-          <div onClick={props.submitHandler} className=" btn uif_button">
-            <span style={{ fontWeight: 'bold', fontSize: '1.4rem' }}>
-              저장하기
-            </span>
+        <div className="d-block text-center mt-5">
+          <div
+            // onClick={props.submitHandler}
+            onClick={props.handleNext}
+            className="btn"
+            style={{
+              textAlign: 'center',
+              margin: '0 auto',
+              padding: '0.3rem 2rem',
+              color: 'white',
+              backgroundColor: '#de6966',
+              boxShadow: '2px 3px 10px rgba(0,0,0,0.5)',
+              fontSize: '1.4rem',
+              lineHeight: 2
+            }}
+          >
+            저장하기
           </div>
         </div>
       </div>
@@ -50,4 +108,4 @@ const Step1 = props => {
   );
 };
 
-export default Step1;
+export default Step2;
